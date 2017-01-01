7596733ae2b74ff3abb9730c87791fa77f4c8a58
gwlim <alphasparc@gmail.com>
2017-01-01 14:36:38 +0800

    Remove unalign patch reduce performance for mips74kc

--- patch_LEDE.sh
+++ patch_LEDE.sh
@@ -4,6 +4,7 @@ set -e
 ./scripts/feeds install -a
 echo Remove Support for PPPOA
 rm ./feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_pppoa.lua
+rm ./target/linux/ar71xx/patches-4.4/910-unaligned_access_hacks.patch
 echo Remove Support for DIR-825 and AllNet Devices
 rm ./target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
 rm ./target/linux/ar71xx/base-files/lib/upgrade/allnet.sh
