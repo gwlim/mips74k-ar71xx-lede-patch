3f6fa630bf3a76a91d815488064aea0e3b0b975d
gwlim <alphasparc@gmail.com>
2017-06-21 23:01:11 +0800

    Add missing folder creation

--- patch_LEDE.sh
+++ patch_LEDE.sh
@@ -12,6 +12,7 @@ echo Clone QCA SRC
 git clone https://source.codeaurora.org/quic/qsdk/oss/lklm/qca-ssdk.git ./feeds/ssdk/qca-ssdk/src -b release/endive_mips
 git clone https://source.codeaurora.org/quic/qsdk/oss/ssdk-shell.git ./feeds/ssdk/qca-ssdk-shell/src -b release/endive_mips
 git clone https://source.codeaurora.org/quic/qsdk/oss/lklm/shortcut-fe.git ./feeds/shortcutfe/src -b release/endive_mips
+mkdir ./feeds/shortcutfe/fast-classifier/src ./feeds/shortcutfe/shortcut-fe/src ./feeds/shortcutfe/simulated-driver/src
 mv ./feeds/shortcutfe/src/fast-classifier/* ./feeds/shortcutfe/fast-classifier/src/
 mv ./feeds/shortcutfe/src/shortcut-fe/* ./feeds/shortcutfe/shortcut-fe/src/
 mv ./feeds/shortcutfe/src/simulated-driver/* ./feeds/shortcutfe/simulated-driver/src/
