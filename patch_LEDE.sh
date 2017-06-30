097d3566a88e602e30384336b042e1cc9a600787
gwlim <alphasparc@gmail.com>
2017-06-30 21:41:55 +0800

    Remove AICCU Service is obsolete

--- patch_LEDE.sh
+++ patch_LEDE.sh
@@ -24,10 +24,13 @@ sed -i 's|+kmod-ipt-extra +kmod-ipt-filter +kmod-ipv6 |+kmod-ipt-extra +kmod-ipt
 ./scripts/feeds install -a
 echo Remove Support for PPPOA
 rm ./feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_pppoa.lua
-rm ./target/linux/ar71xx/patches-4.4/910-unaligned_access_hacks.patch
+echo Remove AICCU Obsolete
+rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/network/proto_aiccu.luas
+rm ./feeds/luci/protocols/luci-proto-ipv6/luasrc/model/cbi/admin_network/proto_aiccu.lua
 echo Remove Support for DIR-825 and AllNet Devices
 rm ./target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
 rm ./target/linux/ar71xx/base-files/lib/upgrade/allnet.sh
+rm ./target/linux/ar71xx/base-files/lib/upgrade/merakinand.sh
         for i in $( ls patch ); do
             echo Applying patch $i
             patch -p1 < patch/$i
